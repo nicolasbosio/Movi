82485a37cc48867b7ef11107683c3fb5d7417d26
Nicolas Bosio <nicoobosio@gmail.com>
2016-09-27 15:55:58 -0300

    adding bikes and tests

--- test/TestTarjeta.php
+++ test/TestTarjeta.php
@@ -17,39 +17,40 @@ class TarjetaTest extends TestCase {
     $tarjeta->recargar(272);
     $this->assertEquals($tarjeta->saldo(), 320, "Cuando cargo $272 deberia tener finalmente $320");
   }
+
   public function testCargaSaldo1() {
     $tarjeta->recargar(600);
     $this->assertEquals($tarjeta->saldo(), 720, "Cuando cargo $600 deberia tener finalmente $720");
   }
+
   public function testCargaSaldo2() {
     $tarjeta->recargar(50);
     $this->assertEquals($tarjeta->saldo(), 50, "Cuando cargo $50 deberia tener finalmente $50");
   }
 
-
   public function testPagarViaje() {
     $tarjeta->recargar(20);
     $tarjeta->pagarColectivo($colectivo1, "2016-09-27 03:12:44");
-    $this->assertEquals($tarjeta->saldo(), 11.5, "Al pagar un viaje con $20, deberia quedarme $11.5")
+    $this->assertEquals($tarjeta->saldo(), 11.5, "Al pagar un viaje con $20, deberia quedarme $11.5");
   }
 
   public function testPagarViajeSinSaldo() {
     $tarjeta->pagarColectivo($colectivo1, "2016-09-27 03:12:44");
-    $this->assertEquals($tarjeta->saldo(),-8.5, "Al pagar un viaje sin saldo, debería descontarme $8.5")
+    $this->assertEquals($tarjeta->saldo(),-8.5, "Al pagar un viaje sin saldo, debería descontarme $8.5");
   }
 
   public function testTransbordo() {
     $tarjeta->recargar(20);
     $tarjeta->pagarColectivo($colectivo1, "2016-09-27 03:12:44");
     $tarjeta->pagarColectivo($colectivo2, "2016-09-27 04:05:44");
-    $this->assertEquals($tarjeta->saldo(), 8.86, "Al pagar dos viajes(trasbordo) con $20, deberia quedarme $8.86")
+    $this->assertEquals($tarjeta->saldo(), 8.86, "Al pagar dos viajes(trasbordo) con $20, deberia quedarme $8.86");
   }
 
   public function testNoTransbordo() {
     $tarjeta->recargar(20);
     $tarjeta->pagarColectivo($colectivo1, "2016-09-27 03:12:44");
     $tarjeta->pagarColectivo($colectivo2, "2016-09-28 03:12:44");
-    $this->assertEquals($tarjeta->saldo(), 3, "Al pagar dos viajes viaje con $20, deberia quedarme $3")
+    $this->assertEquals($tarjeta->saldo(), 3, "Al pagar dos viajes viaje con $20, deberia quedarme $3");
   }
 
 }
