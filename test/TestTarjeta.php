faaaa9255aaf7f937cb1131d2314d4123121cfc1
Nicolas Bosio <nicoobosio@gmail.com>
2016-09-27 13:35:17 -0300

    update4

--- test/TestTarjeta.php
+++ test/TestTarjeta.php
@@ -12,6 +12,16 @@ class TarjetaTest extends TestCase {
     $tarjeta->recargar(272);
     $this->assertEquals($tarjeta->saldo(), 320, "Cuando cargo 272 deberia tener finalmente 320");
   }
+  public function testCargaSaldo1() {
+    $tarjeta = new Sube;
+    $tarjeta->recargar(60);
+    $this->assertEquals($tarjeta->saldo(), 720, "Cuando cargo 600 deberia tener finalmente 720");
+  }
+  public function testCargaSaldo2() {
+    $tarjeta = new Sube;
+    $tarjeta->recargar(50);
+    $this->assertEquals($tarjeta->saldo(), 320, "Cuando cargo 50 deberia tener finalmente 50");
+  }
 
 
   public function testPagarViaje() {
