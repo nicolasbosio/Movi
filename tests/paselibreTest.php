465f84a3fa4836759debee2993f2155a70e75552
Nicolas Bosio <nicoobosio@gmail.com>
2016-10-08 18:46:14 -0300

    fixing small errors

--- tests/paselibreTest.php
+++ tests/paselibreTest.php
@@ -8,28 +8,28 @@ use PHPUnit\Framework\TestCase;
 class paselibreTest extends TestCase {
 
   public function testCargaSaldo() {
-    $tarjeta = new Sube;
+    $tarjeta = new PaseLibre;
 
     $tarjeta->recargar(290);
     $this->assertEquals($tarjeta->saldo(), 340, "Cuando cargo $290 deberia tener finalmente $340");
   }
 
   public function testCargaSaldo1() {
-    $tarjeta = new Sube;
+    $tarjeta = new PaseLibre;
 
     $tarjeta->recargar(544);
     $this->assertEquals($tarjeta->saldo(), 680, "Cuando cargo $544 deberia tener finalmente $680");
   }
 
   public function testCargaSaldo2() {
-    $tarjeta = new Sube;
+    $tarjeta = new PaseLibre;
 
     $tarjeta->recargar(50);
     $this->assertEquals($tarjeta->saldo(), 50, "Cuando cargo $50 deberia tener finalmente $50");
   }
 
   public function testPagarViaje() {
-    $tarjeta = new Sube;
+    $tarjeta = new PaseLibre;
     $colectivo1 = new Colectivo("145", "Rosario Bus");
 
     $tarjeta->recargar(20);
@@ -38,7 +38,7 @@ class paselibreTest extends TestCase {
   }
 
   public function testPagarViajeSinSaldo() {
-    $tarjeta = new Sube;
+    $tarjeta = new PaseLibre;
     $colectivo1 = new Colectivo("145", "Rosario Bus");
 
     $tarjeta->pagar($colectivo1, "2016-09-27 03:12:44");
@@ -46,7 +46,7 @@ class paselibreTest extends TestCase {
   }
 
   public function testTransbordo() {
-    $tarjeta = new Sube;
+    $tarjeta = new PaseLibre;
     $colectivo1 = new Colectivo("145", "Rosario Bus");
     $colectivo2 = new Colectivo("115", "Empresa Mixta de Transporte Rosario");
 
@@ -57,7 +57,7 @@ class paselibreTest extends TestCase {
   }
 
   public function testNoTransbordo() {
-    $tarjeta = new Sube;
+    $tarjeta = new PaseLibre;
     $colectivo1 = new Colectivo("145", "Rosario Bus");
     $colectivo2 = new Colectivo("115", "Empresa Mixta de Transporte Rosario");
 
