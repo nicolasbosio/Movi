5b2a6a0fa7fc2d69eb55fd336b328b465e0ffc70
Nicolas Bosio <nicoobosio@gmail.com>
2016-10-08 18:37:04 -0300

    fixing classes

--- tests/viajeTest.php
+++ tests/viajeTest.php
@@ -4,13 +4,15 @@ namespace Poli\Tarjeta;
 
 use PHPUnit\Framework\TestCase;
 
-class viajeTest extends TestCase{
-	$colectivo1 = new Colectivo("145", "Rosario Bus");
+class viajeTest extends TestCase {
+	public function testFuncionesViaje(){
+		$colectivo1 = new Colectivo("145", "Rosario Bus");
 
-	$a = new Viaje($colectivo1->tipo, 8.5, $colectivo1, "2016-09-27 03:12:44");
+		$a = new Viaje($colectivo1->tipo, 8.5, $colectivo1, "2016-09-27 03:12:44");
 
-	$this->assertEquals($a->tipo(), "colectivo", "es un colectivo");
-	$this->assertEquals($a->monto(), "8.5", "el monto del viaje es 8.5");
-	$this->assertEquals($a->transporte(), $colectivo1, "un 145 de Rosario Bus");
-	$this->assertEquals($a->tiempo(), "2016-09-27 03:12:44", "el viaje se efectuo a las 2016-09-27 03:12:44");
+		$this->assertEquals($a->tipo(), "colectivo", "es un colectivo");
+		$this->assertEquals($a->monto(), "8.5", "el monto del viaje es 8.5");
+		$this->assertEquals($a->transporte(), $colectivo1, "un 145 de Rosario Bus");
+		$this->assertEquals($a->tiempo(), "2016-09-27 03:12:44", "el viaje se efectuo a las 2016-09-27 03:12:44");
+	}
 }
