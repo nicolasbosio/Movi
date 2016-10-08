844dad7b79685f02a683fecf85ff2d9015cf7cc0
Nicolas Bosio <nicoobosio@gmail.com>
2016-10-08 18:29:48 -0300

    adding tests

--- tests/colectivoTest.php
+++ tests/colectivoTest.php
@@ -6,7 +6,7 @@ namespace Poli\Tarjeta;
 use PHPUnit\Framework\TestCase;
 
 class colectivoTest extends TestCase {
-	public function testNombre{
+	public function testNombre(){
 		$a = new Colectivo("145", "Rosario Bus");
 
 		$this->assertEquals($a->Nombre(), "145", "El nombre del colectivo creado es 145");
