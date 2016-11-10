135e48cfbfd11fd116d2f9efd4f10bb3ab3d5a44
guhiter <nicoobosio@gmail.com>
2016-11-10 09:29:39 -0300

    100%

--- tests/trasbordoTest.php
+++ tests/trasbordoTest.php
@@ -46,4 +46,14 @@ class trasbordoTest extends TestCase {
   		$tarjeta->pagar($colectivo2, "2016-11-13 11:22:44");
    		$this->assertEquals($tarjeta->saldo(), 8.86, "Al pagar dos viajes con $20, deberia quedarme $8.86");
 	}
+	public function testTrasbordoNormal(){
+		$tarjeta = new Sube("001");
+		$colectivo1 = new Colectivo("145", "Rosario Bus");
+		$colectivo2 = new Colectivo("115", "Empresa Mixta de Transporte Rosario");
+
+		$tarjeta->recargar(20);
+  		$tarjeta->pagar($colectivo1, "2016-11-10 10:12:44");
+  		$tarjeta->pagar($colectivo2, "2016-11-10 10:52:44");
+   		$this->assertEquals($tarjeta->saldo(), 8.86, "Al pagar dos viajes con $20, deberia quedarme $8.86");
+	}
 }
